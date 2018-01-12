f00d7f6f5649c6fc89a8ac3d9dde72a65fcee4b0
Melody <Melody@Melodys-MacBook.local>
2018-01-12 15:47:39 -0700

    login component

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const massive = require('massive');
 const session = require('express-session');
+const axios = require('axios');
  
 require('dotenv').config();
  
@@ -19,6 +20,13 @@ massive(process.env.CONNECTION_STRING).then(db => {
 }).catch(error => {
     console.log('error', error)
 });
+
+axios.post('/login', (req, res) ={
+    const { userId } = req.body 
+    const auth0Url = `https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/users/${userId}`
+
+    axios.get(auth0Url, {headers: {Authorization: "Bearer" + process.env.AUTH0_MANAGEMENT_ACCESS_TOKEN}})
+})
  
 const port = process.env.SERVER_PORT
 app.listen(port, () => console.log('listening on port ' + port));
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
 import logo from '../../images/logo.png';
-import { Link } from 'react-router-dom';
+import { login } from '../../ducks/reducer';
+import { connect } from 'react-redux';
+import Auth0Lock from 'auth0-lock';
+import axios from 'axios';
 import './Login.css';
 
 class Login extends Component {
+  constructor(){
+    super()
+
+    this.lock = null; 
+    this.login = this.login.bind(this)
+  }
+
+  componentDidMount() {
+    this.lock = new Auth0Lock(process.env.REACT_APP_AUTH0_CLIENT_ID, process.env.REACT_APP_AUTH0_DOMAIN)
+    this.lock.on('authenticated', authResult => {
+        this.lock.getUserInfo(authResult.accessToken, (error, user) => {
+            axios.post('/login', {userId: user.sub}).then(response => {
+                this.props.login(response.data.user)
+                this.props.history.push('/')
+            })
+        })
+    })
+}
+
+  login(){
+    this.lock.show()
+  }
+
   render() {
     return (
       <div className="main-page">
         <div className="splash">
           <img src={logo} />
           Helo
-          <Link className="dark login-button" to="/">Login/Register</Link>
+          <button className="dark login-button" onClick={this.login}>Login/Register</button>
         </div>
       </div>
     );
   }
 }
 
-export default Login;
+const mapDispatchToProps = {
+  login: login
+}
+
+export default connect(null, mapDispatchToProps)(Login);
