dbc3d29c6e84859d400ce604dcf9d9ad324fb5ce
Melody <Melody@Melodys-MacBook.local>
2018-01-12 11:01:13 -0700

    setup up home and login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       Home
+        <Route exact path='/' component={Home} />
       </div>
     );
   }
