c96eb4ae14480db5a61a7b010cb966ba909cfa8c
Melody <Melody@Melodys-MacBook.local>
2018-01-12 10:18:28 -0700

    server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+const session = require('express-session');
+ 
+require('dotenv').config();
+ 
+const app = express();
+ 
+app.use(bodyParser.json());
+app.use(session({
+secret: process.env.SESSION_SECRET, 
+saveUninitialized: false, 
+resave: false
+}));
+ 
+massive(process.env.CONNECTION_STRING).then(db => {
+app.set('db', db)
+}).catch(error => {
+console.log('error', error)
+});
+ 
+const port = process.env.PORT
+app.listen(port, () => console.log('listening on port ' + port));
